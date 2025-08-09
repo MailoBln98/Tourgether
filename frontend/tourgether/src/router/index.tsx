d91f02dcbada128aa4dc4634ca53003edc54a577
Paul Dubinski <hr.paul.dubinski@gmail.com>
2025-08-09 16:02:19 +0200

    Updating footer to be on the bottom of the page and fixing the upload page.

--- frontend/tourgether/src/router/index.tsx
+++ frontend/tourgether/src/router/index.tsx
@@ -4,21 +4,20 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Home from '../pages/Home'
 import Upload from '../pages/Upload'
-import Login from "../pages/Login";
 
 const AppRouter: React.FC = () => {
   return (
-    <>
+    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <main style={{ flex: 1 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/upload" element={<Upload />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
-  );
-};
+    </div>
+  )
+}
 
-
-export default AppRouter;
+export default AppRouter
